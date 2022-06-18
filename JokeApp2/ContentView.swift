3fb3f69477e97598b19271912c48e7fc73c50698
Raditya <raditya.learner@gmail.com>
2022-06-18 19:38:01 +0800

    Initial Commit

---
+++ JokeApp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JokeApp2
+//
+//  Created by CTSS Students on 18/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JokeApp2/JokeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  JokeApp2App.swift
+//  JokeApp2
+//
+//  Created by CTSS Students on 18/6/22.
+//
+
+import SwiftUI
+
+@main
+struct JokeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
